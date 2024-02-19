552c566bceec359023b3781eeec55efbfef62272
godzzz <g0dzZz1lla@yandex.ru>
2024-02-20 03:36:09 +0400

    [v0.0.6] Added background support

--- Runtime/LoadingCurtainWithProgressBar.cs
+++ Runtime/LoadingCurtainWithProgressBar.cs
@@ -2,14 +2,17 @@
 // © 2023-2024 Nikolay Melnikov <n.melnikov@depra.org>
 
 using Depra.Loading.Operations;
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace Depra.Loading
 {
+	[DisallowMultipleComponent]
 	public sealed class LoadingCurtainWithProgressBar : LoadingCurtainView
 	{
 		[SerializeField] private UIDocument _document;
+		[CanBeNull] [SerializeField] private Sprite _background;
 		[SerializeField] private string _descriptionName = "Description";
 		[SerializeField] private string _progressBarName = "ProgressBar";
 
@@ -33,6 +36,11 @@ namespace Depra.Loading
 			var rootElement = _document.rootVisualElement;
 			_descriptionLabel = rootElement.Q<Label>(_descriptionName);
 			_progressBar = rootElement.Q<ProgressBar>(_progressBarName);
+
+			if (_background)
+			{
+				rootElement.style.backgroundImage = new StyleBackground(_background);
+			}
 
 			_viewModel = viewModel;
 			_viewModel.Progress.Changed += OnProgressChanged;
